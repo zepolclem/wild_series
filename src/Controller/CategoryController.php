d402f9c28d9c39dd59c8cc74deca9fe132529ba8
zepolclem <lopez.clmnt@gmail.com>
2021-04-22 16:30:37 +0200

    quest_11 + fix paramConvert programs

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Entity\Program;
+use App\Form\CategoryType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -27,6 +29,36 @@ class CategoryController extends AbstractController
         return $this->render('category/index.html.twig',
             ['categories' => $categories]
         );
+    }
+
+    /**
+     * The controller for the category add form
+     * Display the form or deal with it.
+     *
+     * @Route("/new", name="new")
+     */
+    public function new(Request $request): Response
+    {
+        // Create a new Category Object
+        $category = new Category();
+        // Create the associated Form
+        $form = $this->createForm(CategoryType::class, $category);
+        // Get data from HTTP request
+        $form->handleRequest($request);
+        // Was the form submitted ?
+        if ($form->isSubmitted()) {
+            // Deal with the submitted data
+            // Get the Entity Manager
+            $entityManager = $this->getDoctrine()->getManager();
+            // Persist Category Object
+            $entityManager->persist($category);
+            // Flush the persisted object
+            $entityManager->flush();
+            // Finally redirect to categories list
+            return $this->redirectToRoute('category_index');
+        }
+        // Render the form
+        return $this->render('category/new.html.twig', ['form' => $form->createView()]);
     }
 
     /**
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 use App\Entity\Episode;
 use App\Entity\Program;
 use App\Entity\Season;
+use App\Form\ProgramType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -29,6 +31,33 @@ class ProgramController extends AbstractController
         return $this->render('program/index.html.twig',
             ['programs' => $programs]
         );
+    }
+
+    /**
+     * @Route("/new", name="new")
+     */
+    public function new(Request $request): Response
+    {
+        // Create a new Program Object
+        $program = new Program();
+        // Create the associated Form
+        $form = $this->createForm(ProgramType::class, $program);
+        // Get data from HTTP request
+        $form->handleRequest($request);
+        // Was the form submitted ?
+        if ($form->isSubmitted()) {
+            // Deal with the submitted data
+            // Get the Entity Manager
+            $entityManager = $this->getDoctrine()->getManager();
+            // Persist Program Object
+            $entityManager->persist($program);
+            // Flush the persisted object
+            $entityManager->flush();
+            // Finally redirect to categories list
+            return $this->redirectToRoute('program_index');
+        }
+        // Render the form
+        return $this->render('category/new.html.twig', ['form' => $form->createView()]);
     }
 
     /**
@@ -58,20 +87,18 @@ class ProgramController extends AbstractController
      * Show season by program id and season id.
      *
      * @Route("/{program_id}/seasons/{season_id} ", name="show_season", requirements={"program"="\d+", "season"="\d+"})
-     * @ParamConverter("program", class="App\Entity\Program", options={"mapping": {"program_id": "id"}})
-     * @ParamConverter("season", class="App\Entity\Season", options={"mapping": {"season_id": "id"}})
+     * @ParamConverter("program", class="App\Entity\Program", options={"id": "program_id"})
      */
-    public function showSeason(program $program, season $season)
+    public function showSeason(program $program, int $season_id)
     {
-        // $program = $this->getDoctrine()->getRepository(Program::class)->findOneBy(['id' => $programId]);
-        // $season = $this->getDoctrine()->getRepository(Season::class)->findOneBy(['id' => $seasonId]);
+        $season = $this->getDoctrine()->getRepository(Season::class)->findOneBy(['number' => $season_id, 'program' => $program->getId()]);
 
         if (!$program) {
             throw $this->createNotFoundException('No program with id : '.$program->getId().' found in program\'s table.');
         }
 
         if (!$season) {
-            throw $this->createNotFoundException('No program with id : '.$season->getId().' found in season\'s table.');
+            // throw $this->createNotFoundException('No program with id : '.$season->getId().' found in season\'s table.');
         }
 
         return $this->render('program/season_show.html.twig', [
@@ -81,13 +108,14 @@ class ProgramController extends AbstractController
     }
 
     /**
-     * @Route("/programs/{program_id}/seasons/{season_id}/episodes/{episode_id}", name="episode_show")
+     * @Route("/{program_id}/seasons/{season_id}/episodes/{episode_id}", name="episode_show")
      * @ParamConverter("program", class="App\Entity\Program", options={"mapping": {"program_id": "id"}})
-     * @ParamConverter("season", class="App\Entity\Season", options={"mapping": {"season_id": "id"}})
-     * @ParamConverter("episode", class="App\Entity\Episode", options={"mapping": {"episode_id": "id"}})
      */
-    public function showEpisode(Program $program, Season $season, Episode $episode)
+    public function showEpisode(Program $program, int $season_id, int $episode_id)
     {
+        $season = $this->getDoctrine()->getRepository(Season::class)->findOneBy(['program' => $program->getId(), 'number' => $season_id]);
+        $episode = $this->getDoctrine()->getRepository(Episode::class)->findOneBy(['season' => $season->getId(), 'number' => $episode_id]);
+
         return $this->render('program/episode_show.html.twig', [
             'program' => $program,
             'season' => $season,
